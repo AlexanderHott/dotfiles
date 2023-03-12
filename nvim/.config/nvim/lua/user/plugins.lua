f7762d0579ca2caac40ab4be00eca4bd19deb25c
AlexanderHOtt <aott01@protonmail.ch>
2023-03-11 22:14:53 -0800

    add rest.nvim

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -38,7 +38,12 @@ require("lazy").setup({
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
+        build = ":TSUpdate",
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = {"http", "json", "rust", "toml", "lua"}
+            }
+        end
     },
     { 'VonHeikemen/lsp-zero.nvim',
         dependencies = {
@@ -93,4 +98,25 @@ require("lazy").setup({
             pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
         })
     end,
+    {
+        "rest-nvim/rest.nvim"
+        config = function()
+            require("rest-nvim").setup({
+                -- Open request results in a horizontal split
+                result_split_horizontal = false,
+
+                -- Skip SSL verification, useful for unknown certificates
+                skip_ssl_verification = false,
+
+                -- Highlight request on run
+                highlight = {
+                    enabled = true,
+                    timeout = 150,
+                },
+
+                -- Jump to request line on run
+                jump_to_request = false,
+            })
+        end,
+    },
 }})
